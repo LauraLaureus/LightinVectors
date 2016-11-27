95b875189eee5979ec4d5ab1b18f87117993de0f
Laura <l4depende@gmail.com>
2016-11-27 16:10:29 +0100

    Light intensity in one point

--- LightingVectors/main.cpp
+++ LightingVectors/main.cpp
@@ -8,8 +8,39 @@
 
 #include <iostream>
 
+
+using namespace std;
+
+
+
+float calculateDotProduct(float surfaceNormal[], float ray[]){
+    
+    return surfaceNormal[0]*ray[0]+surfaceNormal[1]*ray[1]+surfaceNormal[2]*ray[2];
+}
+
+
+
+void calculateRay(float destiny[], float lightSource[],float* result){
+    
+    for (int i = 0; i < 3; i++) {
+        result[i] = lightSource[i] - destiny[i];
+    }
+    
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    
+    float normal2surface[3] = {0,1,0};
+    float illuminatedPoint[3] = {0,10,0};
+    float lightSource[3] = {20,20,40};
+    
+    float ray[3];
+    calculateRay(illuminatedPoint,lightSource,ray);
+    
+    float intensityOfLight = calculateDotProduct(normal2surface,ray);
+    
+    cout << intensityOfLight << endl;
+    
     return 0;
 }
