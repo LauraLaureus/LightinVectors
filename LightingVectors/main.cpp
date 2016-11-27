02bf682d65982187b254498af948072c423a5944
Laura <l4depende@gmail.com>
2016-11-27 15:19:43 +0100

    Initial Commit

---
+++ LightingVectors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LightingVectors
+//
+//  Created by Laura del Pino Díaz on 27/11/16.
+//  Copyright © 2016 Laura del Pino Díaz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
